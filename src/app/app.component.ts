9c7df40651ce56ebe905df3df0b150abf9419340
Marty Trujillo <marty@ilikeemail.com>
2024-05-14 14:38:18 -0400

    Video 13 recognition start.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare var webkitSpeechRecognition: any;
+declare var SpeechRecognition: any;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +12,35 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'voice';
+  recognition: any;
+
+  constructor() {
+    if ('SpeechRecongition' in window) {
+      this.recognition =  new SpeechRecognition();
+      console.log('Speech Recognition Exists');
+    }  else if ('webkitSpeechRecognition' in window) {
+      this.recognition =  new webkitSpeechRecognition();
+      console.log('Using Webkit Speech Recognition');
+    }
+
+    if(this.recognition) {
+      this.recognition.continuous = false;
+      //this.recognition.lang = 'en-US';
+      this.recognition.interimResults = false;
+      this.recognition.maxAlternatives = 1;
+
+      console.log('default lang',this.recognition.lang);
+
+      this.recognition.start();
+
+      this.recognition.onresult = (result: any) => {
+        console.log('result', result);
+      }
+    } else {
+      console.log('Speech recognition not supported!');
+    }
+
+    console.log('recognition',this.recognition);
+
+  }
 }
