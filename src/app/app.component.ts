a6eadd7fc2b62e5a422a420fd2b17ea6010f5245
Marty Trujillo <marty@ilikeemail.com>
2024-05-17 14:00:09 -0400

    Video 015

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,7 +8,7 @@ declare var SpeechRecognition: any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,6 +17,9 @@ export class AppComponent {
 
   result: string = '';
 
+  colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+
+  currentColor = 'beige';
   constructor(private changeDetectorRef: ChangeDetectorRef) {
     if ('SpeechRecongition' in window) {
       this.recognition =  new SpeechRecognition();
@@ -43,6 +47,7 @@ export class AppComponent {
 
   setResult(res: string) {
     this.result = res;
+    this.currentColor = res;
     this.changeDetectorRef.detectChanges();
   }
 }
